201f3f2c5e5c9b98754db8058beb78f80bd28eeb
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 16:30:59 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//
+fetch('https://api64.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('ip').innerHTML = 
+            `IP for router448461.com and bonjour.router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+    })
+    .catch(error => console.error('Error:', error));
