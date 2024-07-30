c36acfec0853e277033d9a64b816b9f8aedf0508
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 17:44:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,16 @@ window.onload = function() {
             .then(response => response.json())
             .then(data => {
                 document.getElementById('ip1').innerHTML = 
-                    `IPv4 for router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
-                    `IPv6 for router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
+                    `IP for router448461.com: <br> <span style="color:red">${data.ip}</span>`;
             })
             .catch(error => console.error('Error:', error));
-    }, 3000);  // Delay of 5 seconds
 
-    setTimeout(function() {
         fetch('https://api64.ipify.org?format=json')
             .then(response => response.json())
             .then(data => {
                 document.getElementById('ip2').innerHTML = 
-                    `IPv4 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
-                    `IPv6 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
+                    `IP for bonjour.router448461.com: <br> <span style="color:red">${data.ip}</span>`;
             })
             .catch(error => console.error('Error:', error));
-    }, 3000);  // Delay of 5 seconds
+    }, 2000);  // Delay of 2 seconds
 }
