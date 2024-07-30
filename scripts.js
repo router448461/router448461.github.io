bf1b34921460453b7072abaee17a7eb456cd7d0b
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 10:19:07 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-/*
-
-*/
+var map = L.map('map').setView([51.505, -0.09], 2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
