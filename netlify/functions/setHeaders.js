3fc0381e865baa80e48c56980979e52d93e3a62e
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 16:32:02 +1000

    Delete netlify directory

--- netlify/functions/setHeaders.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    headers: { 'X-Content-Type-Options': 'nosniff' },
-    body: JSON.stringify({ message: "Header set" })
-  };
-}
