12e0fcd0a35a8f3b19ab3eaab4bc7a6d026fbe8d
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 18:08:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 window.onload = function() {
-    setTimeout(function() {
-        fetch('https://api64.ipify.org?format=json')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('ip1').innerHTML = 
-                    `IPv4 for router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
-                    `IPv6 for router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
-            })
-            .catch(error => console.error('Error:', error));
-    }, 1000);
+    var displayArea = document.getElementById('display-area');
 
-    setTimeout(function() {
-        fetch('https://api64.ipify.org?format=json')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('ip2').innerHTML = 
-                    `IPv4 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
-                    `IPv6 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
-            })
-            .catch(error => console.error('Error:', error));
-    }, 1000);
-}
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            displayArea.innerHTML = 'Your IP Address is: ' + data.ip;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+};
