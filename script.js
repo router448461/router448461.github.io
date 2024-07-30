dba3bfdab271286625586188312ad677869b0794
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 17:15:38 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 window.onload = function() {
-    fetch('https://api64.ipify.org?format=json')
+    fetch('https://your-api-url-here')
         .then(response => response.json())
         .then(data => {
             document.getElementById('ip1').innerHTML = 
-                `IP for router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+                `IPv4 for router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
+                `IPv6 for router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
         })
         .catch(error => console.error('Error:', error));
 
-    fetch('https://api64.ipify.org?format=json')
+    fetch('https://your-api-url-here')
         .then(response => response.json())
         .then(data => {
             document.getElementById('ip2').innerHTML = 
-                `IP for bonjour.router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+                `IPv4 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
+                `IPv6 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
         })
         .catch(error => console.error('Error:', error));
 }
