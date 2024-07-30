3d6f951b43da364cb58e5190ab887d178326c3d1
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 17:01:39 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-fetch('https://api64.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('ip1').innerHTML = 
-            `IP for router448461.com: <br> <span style="color:red">${data.ip}</span>`;
-    })
-    .catch(error => console.error('Error:', error));
+window.onload = function() {
+    fetch('https://api64.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('ip1').innerHTML = 
+                `IP for router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+        })
+        .catch(error => console.error('Error:', error));
 
-fetch('https://api64.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('ip2').innerHTML = 
-            `IP for bonjour.router448461.com: <br> <span style="color:red">${data.ip}</span>`;
-    })
-    .catch(error => console.error('Error:', error));
+    fetch('https://api64.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('ip2').innerHTML = 
+                `IP for bonjour.router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+        })
+        .catch(error => console.error('Error:', error));
+}
