584224fe9185ab375301e27fba601ffba4f13ebe
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 17:19:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = function() {
-    fetch('https://your-api-url-here')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('ip1').innerHTML = 
-                `IPv4 for router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
-                `IPv6 for router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
-        })
-        .catch(error => console.error('Error:', error));
+    setTimeout(function() {
+        fetch('https://api64.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('ip1').innerHTML = 
+                    `IP for router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+            })
+            .catch(error => console.error('Error:', error));
 
-    fetch('https://your-api-url-here')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('ip2').innerHTML = 
-                `IPv4 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv4}</span><br>` +
-                `IPv6 for bonjour.router448461.com: <br> <span style="color:red">${data.ipv6}</span>`;
-        })
-        .catch(error => console.error('Error:', error));
+        fetch('https://api64.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('ip2').innerHTML = 
+                    `IP for bonjour.router448461.com: <br> <span style="color:red">${data.ip}</span>`;
+            })
+            .catch(error => console.error('Error:', error));
+    }, 3000);  // Delay of 2 seconds
 }
