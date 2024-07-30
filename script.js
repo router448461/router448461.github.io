e914298cbe4f0aea405727fb59cdd55fe60f5525
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-07-30 13:10:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+// Create a projection
+var projection = d3.geoMercator();
+
 // Initialize map using D3.js and TopoJSON
 var map = d3.geoPath().projection(projection);
 
 // Load GeoIP data (you'll need to supply this)
 d3.json("geoip.json", function(error, geoip) {
-  if (error) throw error;
+  if (error) throw error;
 
-  // Draw map
-  svg.append("g")
-    .attr("class", "countries")
-    .selectAll("path")
-    .data(topojson.feature(geoip, geoip.objects.countries).features)
-    .enter().append("path")
-    .attr("d", map);
+  // Draw map
+  svg.append("g")
+    .attr("class", "countries")
+    .selectAll("path")
+    .data(topojson.feature(geoip, geoip.objects.countries).features)
+    .enter().append("path")
+    .attr("d", map);
 });
 
 // Setup WebSocket connection for real-time data
 var socket = new WebSocket("ws://yourserver:yourport");
 
 socket.onmessage = function(event) {
-  var data = JSON.parse(event.data);
+  var data = JSON.parse(event.data);
 
-  // Update map based on data
-  // ...
+  // Update map based on data
+  svg.append("circle")
+    .attr("cx", function(d) { return projection([data.longitude, data.latitude])[0]; })
+    .attr("cy", function(d) { return projection([data.longitude, data.latitude])[1]; })
+    .attr("r", "5px")
+    .attr("fill", "red");
 };
