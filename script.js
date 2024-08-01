1f1834cd60b6e72dfd1f63cdd518477301215def
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-08-01 21:55:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ window.onload = function() {
     var displayArea = document.getElementById('display-area');
     var map = L.map('map', { zoomControl: false, dragging: false, attributionControl: false, scrollWheelZoom: false, doubleClickZoom: false, boxZoom: false, keyboard: false }).setView([0, 0], 3);
 
-    // Use a dark mode tile layer
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: ''
     }).addTo(map);
 
