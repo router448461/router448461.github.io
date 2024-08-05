242726047c096054a51ef2dabdcdc71176c8f4f4
JOSHUA esk <175668725+router448461@users.noreply.github.com>
2024-08-05 20:04:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -23,9 +23,6 @@ window.onload = function() {
 
     map.scrollWheelZoom.disable();
 
-    var ipCoords = [37.7749, -122.4194];
-    var doverCoords = [-43.3167, 147.0167];
-
     var nameServers = [
         'ns8.dynu.com', 'ns9.dynu.com', 'ns7.dynu.com', 'ns1.dynu.com', 'ns5.dynu.com', 'ns2.dynu.com', 'ns4.dynu.com', 'ns3.dynu.com', 'ns6.dynu.com', 'ns12.dynu.com', 'ns10.dynu.com', 'ns11.dynu.com'
     ];
@@ -34,9 +31,11 @@ window.onload = function() {
         [51.5074, -0.1278], [48.8566, 2.3522], [52.5200, 13.4050], [34.0522, -118.2437], [35.6895, 139.6917], [55.7558, 37.6173], [40.730610, -73.935242], [39.9042, 116.4074], [28.6139, 77.2090], [37.5665, 126.9780], [31.2304, 121.4737], [22.3964, 114.1095]
     ];
 
+    var doverCoords = [-43.3167, 147.0167];
+
     var polyline = L.polyline([], {color: 'blue', weight: 1}).addTo(map);
 
-    var latlngs = [ipCoords].concat(nameServerCoords).concat([doverCoords]);
+    var latlngs = nameServerCoords.concat([doverCoords]);
     var totalDuration = 30000;
     var steps = 100;
     var interval = totalDuration / steps;
@@ -71,11 +70,11 @@ window.onload = function() {
         ipInfo.innerHTML = ipAddresses.join('<br>');
     });
 
-    var coords = [ipCoords].concat(nameServerCoords).concat([doverCoords]);
-    coords.forEach(function(coord, index) {
+    var coords = nameServerCoords.concat([doverCoords]);
+    coords.forEach(function(coord) {
         var dot = L.divIcon({
             className: 'dot',
-            html: `<div style="background-color: ${index < 1 ? 'green' : 'red'}; width: 1px; height: 1px; border-radius: 50%; animation: blink 1s infinite;"></div>`
+            html: `<div style="background-color: red; width: 1px; height: 1px; border-radius: 50%; animation: blink 1s infinite;"></div>`
         });
         L.marker(coord, { icon: dot }).addTo(map);
     });
