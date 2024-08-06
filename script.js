2c8eb84bdef1708699b241ff76407a7cc10237eb
JOSHUA esk <joshua.esk@icloud.com>
2024-08-06 17:55:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,6 @@ window.onload = function() {
         noWrap: true
     }).addTo(map);
 
-    // Add Leaflet.Perspective plugin for 3D view
-    L.perspective(map, {
-        pitch: 45,
-        bearing: 0
-    });
-
     map.on('zoomend', function() {
         map.setZoom(2);
     });
