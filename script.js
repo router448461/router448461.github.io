4a9366852617268468adbd023293364b85eefa8f
JOSHUA esk <hidemyemail@router448461.com>
2024-08-09 19:08:06 +1000

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ window.onload = function() {
         return item.coords;
     })).concat([hobartCoords]);
 
-    var polyline = L.polyline([], {color: '#4b5320', weight: 1}).addTo(map);
+    var polyline = L.polyline([], {color: '#4b5320', weight: 3}).addTo(map);
 
     var totalDuration = 30000;
     var steps = 100;
