847244dbd45a8d2a50e23bbecc0f2b75a8ee1e4e
JOSHUA esk <joshua.esk@icloud.com>
2025-03-28 18:20:03 +1100

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const militaryBases = [
+    { name: "HMAS Creswell", lat: -35.0589, lon: 150.6629 },
+    { name: "HMAS Stirling", lat: -32.2490, lon: 115.6840 },
+    { name: "Larrakeyah Barracks", lat: -12.4572, lon: 130.8338 },
+    { name: "Gallipoli Barracks", lat: -27.4064, lon: 152.9731 },
+    { name: "RAAF Base Amberley", lat: -27.6269, lon: 152.7111 },
+    { name: "Anglesea Barracks", lat: -42.8917, lon: 147.3272 },
+    { name: "Victoria Barracks", lat: -37.8315, lon: 144.9780 },
+    { name: "Campbell Barracks", lat: -31.9523, lon: 115.7585 },
+    // Add more bases as needed
+];
