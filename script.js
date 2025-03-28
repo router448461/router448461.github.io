c2547009373679fc779835713ee5350a481631c9
JOSHUA esk <joshua.esk@icloud.com>
2025-03-28 20:04:47 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const map = L.map('map-container', {
     zoomControl: false, // Disable zoom buttons
     attributionControl: false, // Remove Leaflet attribution
+    worldCopyJump: true // Ensure the world doesn't wrap infinitely
 }).setView([0, 0], 2); // Initial map view at [Lat, Lng]
 
 // Add dark mode tiles without labels using Carto's Positron (no labels) tiles
@@ -9,6 +10,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png'
     attribution: '&copy; <a href="https://carto.com/">Carto</a>',
     subdomains: 'abcd',
     maxZoom: 19,
+    noWrap: true // Prevent horizontal map wrapping
 }).addTo(map);
 
 // Function to center the red crosshair lines
