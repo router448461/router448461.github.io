82292e4a0a0c7d641c5e723604468971ae8eff3e
JOSHUA esk <joshua.esk@icloud.com>
2025-04-02 17:23:24 +1100

    Update script.js

--- Folder/script.js
+++ Folder/script.js
@@ -4,13 +4,13 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm91dGVyNDQ4NDYxIiwiYSI6ImNtOHpoZ2ZzZTBjMDIya
 // Initialize the map
 const map = new mapboxgl.Map({
     container: 'map', // ID of the container where the map is rendered
-    style: 'mapbox://styles/router448461/cm8zh31z3005f01sradm3gkfg', // Your Mapbox style ID
+    style: 'mapbox://styles/router448461/cm8zh31z3005f01sradm3gkfg', // Verified Mapbox style ID
     center: [0, 0], // Center map at coordinates [longitude, latitude]
     zoom: 1,        // Default zoom level
     projection: 'equalEarth' // Set the map projection to Equal Earth
 });
 
-// Disable all mouse interactions
+// Disable all mouse interactions for a static map experience
 map.scrollZoom.disable();  // Disable scroll zoom
 map.boxZoom.disable();     // Disable box zoom
 map.dragRotate.disable();  // Disable drag rotation
