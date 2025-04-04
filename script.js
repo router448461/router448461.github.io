d30e6f314b6c44068a1e4dd853906e28f14115a3
JOSHUA esk <joshua.esk@icloud.com>
2025-04-04 14:44:29 +1100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const map = new mapboxgl.Map({
   attributionControl: false,
 });
 
-// Disable interactive gestures
+// Disable map interactions
 map.dragPan.disable();
 map.dragRotate.disable();
 map.scrollZoom.disable();
@@ -17,6 +17,21 @@ map.boxZoom.disable();
 map.keyboard.disable();
 map.touchZoomRotate.disable();
 
+// A JavaScript glitch effect applied to a given element using random transforms
+function applyGlitchEffect(element, intensity = 2) {
+  function glitch() {
+    const dx = (Math.random() * intensity * 2) - intensity;
+    const dy = (Math.random() * intensity * 2) - intensity;
+    const skew = (Math.random() * intensity * 0.2) - (intensity * 0.1);
+    element.style.transform = `translate(${dx}px, ${dy}px) skew(${skew}deg)`;
+    setTimeout(glitch, Math.random() * 200 + 50);
+  }
+  glitch();
+}
+
 map.on('load', () => {
-  // Additional map logic can be added here if needed.
+  const lens = document.getElementById('lens-effect');
+  if (lens) {
+    applyGlitchEffect(lens, 2);
+  }
 });
