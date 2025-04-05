51f36491e71302067bea6ff88a7c69b74cc7cd19
JOSHUA esk <joshua.esk@icloud.com>
2025-04-05 21:34:03 +1100

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ map.on('load', () => {
     const coords = map.unproject([mouseX, mouseY]);
     const lat = coords.lat.toFixed(3);
     const lng = coords.lng.toFixed(3);
-    document.getElementById('target-coords').innerText = `TARGET: ${lat}, ${lng}`;
+    document.getElementById('target-coords').innerHTML = `<span>LAT. ${lat}</span><span>LON. ${lng}</span>`;
   });
 });
 document.addEventListener('mousemove', function(e) {
@@ -30,3 +30,12 @@ document.addEventListener('mousemove', function(e) {
   reticle.style.left = `${e.clientX}px`;
   reticle.style.top = `${e.clientY}px`;
 });
+function updateTimer() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+  document.getElementById('timer').innerText = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
+setInterval(updateTimer, 10);
