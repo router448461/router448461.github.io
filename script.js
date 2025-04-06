2082fdf17cfecb5ec8f4e7f13247708a58563b84
JOSHUA esk <joshua.esk@icloud.com>
2025-04-06 17:41:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 import { initMap } from './mapModule.js';
 import { startTimer } from './timerModule.js';
+import { bootSequence } from './bootModule.js';
+import { startGlitch } from './glitchModule.js';
 
+bootSequence();
 initMap();
 startTimer();
+startGlitch();
+
 document.addEventListener('mousemove', (e) => {
   const reticle = document.getElementById('reticle');
   reticle.style.left = `${e.clientX}px`;
