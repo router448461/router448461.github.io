0d77274a796f1d5af65091de5e8b7ede6c3f1351
JOSHUA esk <joshua.esk@icloud.com>
2025-04-06 18:06:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,16 @@ import { initMap } from './mapModule.js';
 import { startTimer } from './timerModule.js';
 import { bootSequence } from './bootModule.js';
 import { startGlitch } from './glitchModule.js';
-import { startThreatIndicators } from './threatModule.js';
-import { initSensorModeToggle } from './sensorModeModule.js';
 
 bootSequence();
 initMap();
 startTimer();
 startGlitch();
-startThreatIndicators();
-initSensorModeToggle();
 
 document.addEventListener('mousemove', (e) => {
   const reticle = document.getElementById('reticle');
   reticle.style.left = `${e.clientX}px`;
   reticle.style.top = `${e.clientY}px`;
-
-  // Slight pull effect on the map based on mouse position relative to window center
   const centerX = window.innerWidth / 2;
   const centerY = window.innerHeight / 2;
   const offsetX = - (e.clientX - centerX) * 0.02;
