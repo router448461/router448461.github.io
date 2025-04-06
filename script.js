8cebd7a4de5185c3e0ac7c6887f96f89c8e463e0
JOSHUA esk <joshua.esk@icloud.com>
2025-04-06 18:11:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,4 @@ document.addEventListener('mousemove', (e) => {
   const reticle = document.getElementById('reticle');
   reticle.style.left = `${e.clientX}px`;
   reticle.style.top = `${e.clientY}px`;
-  const centerX = window.innerWidth / 2;
-  const centerY = window.innerHeight / 2;
-  const offsetX = - (e.clientX - centerX) * 0.02;
-  const offsetY = - (e.clientY - centerY) * 0.02;
-  document.getElementById('map').style.transform = `translate(${offsetX}px, ${offsetY}px)`;
 });
