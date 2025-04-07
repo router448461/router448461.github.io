47e6b47c29705346ff7c7877deefa1496f29c6b2
JOSHUA esk <joshua.esk@icloud.com>
2025-04-07 18:20:17 +1000

    Update bootModule.js

--- bootModule.js
+++ bootModule.js
@@ -3,12 +3,9 @@ export function bootSequence() {
   const bootOverlay = document.getElementById('boot-overlay');
   bootOverlay.innerHTML = "ESTABLISHING VIDEO UPLINK...";
   setTimeout(() => {
-    bootOverlay.classList.add('boot-lens');
+    bootOverlay.classList.add('boot-hidden');
     setTimeout(() => {
-      bootOverlay.classList.add('boot-hidden');
-      setTimeout(() => {
-        bootOverlay.remove();
-      }, 1000);
-    }, 1500);
+      bootOverlay.remove();
+    }, 1000);
   }, 4000);
 }
