d5928865de473a64f90c6570986ca1e86e7d3677
JOSHUA esk <joshua.esk@icloud.com>
2025-04-07 15:58:56 +1000

    Update bootModule.js

--- bootModule.js
+++ bootModule.js
@@ -1,10 +1,7 @@
 export function bootSequence() {
   const bootOverlay = document.getElementById('boot-overlay');
   const messages = [
-    "SYSTEM INITIALIZING...",
-    "LOADING SENSOR DATA...",
-    "CALIBRATING TARGETING SYSTEM...",
-    "SYSTEM READY."
+    "ESTABLISHING VIDEO UPLINK...",
   ];
   bootOverlay.innerHTML = messages.join('<br>');
   setTimeout(() => {
