f9396df6c76d7bee375818bfadd91275da589b24
JOSHUA esk <joshua.esk@icloud.com>
2025-04-07 18:59:04 +1000

    Update mapModule.js

--- mapModule.js
+++ mapModule.js
@@ -34,7 +34,7 @@ function throttle(func,limit){
   let lastCall = 0;
   return function(...args){
     const now = Date.now();
-    if(now - lastCall >= limit){
+    if(now - lastCall>= limit){
       lastCall = now;
       func.apply(this,args);
     }
