65da1d985dba6308a19e0c13cc8251c098d9fc50
JOSHUA esk <joshua.esk@icloud.com>
2025-04-07 18:38:02 +1000

    Update bootModule.js

--- bootModule.js
+++ bootModule.js
@@ -2,6 +2,7 @@
 export function bootSequence() {
   const bootOverlay = document.getElementById('boot-overlay');
   bootOverlay.innerHTML = "ESTABLISHING VIDEO UPLINK...";
+  // When the boot screen completes, remove it immediately (without additional lens animation)
   setTimeout(() => {
     bootOverlay.classList.add('boot-hidden');
     setTimeout(() => {
