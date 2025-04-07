cfe9389174fa5de09c3e5ca662e7b530b10f44a4
JOSHUA esk <joshua.esk@icloud.com>
2025-04-07 17:27:54 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // script.js
+import { bootSequence } from './bootModule.js';
 import { initMap } from './mapModule.js';
 import { startTimer } from './timerModule.js';
-import { bootSequence } from './bootModule.js';
 import { startGlitch } from './glitchModule.js';
 
 bootSequence();
-initMap();
-startTimer();
-startGlitch();
-
-document.addEventListener('mousemove', (e) => {
-  const reticle = document.getElementById('reticle');
-  reticle.style.left = `${e.clientX}px`;
-  reticle.style.top = `${e.clientY}px`;
-});
+setTimeout(() => {
+  initMap();
+  startTimer();
+  startGlitch();
+  document.addEventListener('mousemove', (e) => {
+    const reticle = document.getElementById('reticle');
+    reticle.style.left = `${e.clientX}px`;
+    reticle.style.top = `${e.clientY}px`;
+  });
+}, 5000);
