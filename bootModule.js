3ba6bab4a8b571c81e3cea9a300d8a9ff0e60662
Joshua Esk <joshua.esk@icloud.com>
2025-04-14 20:06:06 +1000

    Delete bootModule.js

--- bootModule.js
+++
@@ -1,7 +0,0 @@
-// bootModule.js
-export function bootSequence() {
-  const bootOverlay = document.getElementById('boot-overlay');
-  bootOverlay.innerHTML = "ESTABLISHING VIDEO UPLINK...";
-  bootOverlay.classList.add('boot-start');
-  bootOverlay.addEventListener('animationend', () => bootOverlay.remove());
-}
