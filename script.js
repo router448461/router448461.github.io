040874ddcb95e894e1180bb3f1a415a3d617e811
Joshua Esk <joshua.esk@icloud.com>
2025-04-14 19:54:49 +1000

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,13 @@ setTimeout(() => {
     const reticle = document.getElementById('reticle');
     reticle.style.left = `${e.clientX}px`;
     reticle.style.top = `${e.clientY}px`;
+    const mapContainer = document.getElementById('map');
+    const rect = mapContainer.getBoundingClientRect();
+    if (e.clientX >= rect.left && e.clientX <= rect.right && e.clientY >= rect.top && e.clientY <= rect.bottom) {
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+      const coords = window.map.unproject([mouseX, mouseY]);
+      document.getElementById('coords').innerText = `LAT: ${coords.lat.toFixed(3)}  LON: ${coords.lng.toFixed(3)}`;
+    }
   });
 }, 5000);
