6f466203fbd9d13acca355aaecca760ab29cff1f
Joshua Esk <joshua.esk@icloud.com>
2025-04-14 19:54:37 +1000

    Update mapModule.js

--- mapModule.js
+++ mapModule.js
@@ -8,7 +8,7 @@ export function initMap() {
     zoom: 1.5,
     attributionControl: false
   });
-  
+  window.map = map;
   map.dragPan.disable();
   map.dragRotate.disable();
   map.scrollZoom.disable();
@@ -16,30 +16,4 @@ export function initMap() {
   map.boxZoom.disable();
   map.keyboard.disable();
   map.touchZoomRotate.disable();
-  
-  map.on('load', () => {
-    const mapContainer = map.getContainer();
-    mapContainer.addEventListener('mousemove', throttle(function(e) {
-      const rect = mapContainer.getBoundingClientRect();
-      const mouseX = e.clientX - rect.left;
-      const mouseY = e.clientY - rect.top;
-      const coords = map.unproject([mouseX, mouseY]);
-      const lat = parseFloat(coords.lat);
-      const lng = parseFloat(coords.lng);
-      const formattedLat = (lat >= 0 ? '+' : '-') + String(Math.floor(Math.abs(lat))).padStart(3, '0') + '.' + String(Math.floor((Math.abs(lat) - Math.floor(Math.abs(lat))) * 1000)).padStart(3, '0');
-      const formattedLng = (lng >= 0 ? '+' : '-') + String(Math.floor(Math.abs(lng))).padStart(3, '0') + '.' + String(Math.floor((Math.abs(lng) - Math.floor(Math.abs(lng))) * 1000)).padStart(3, '0');
-      document.getElementById('coords').innerText = `LAT: ${formattedLat}  LON: ${formattedLng}`;
-    }, 50));
-  });
-}
-
-function throttle(func, limit) {
-  let lastCall = 0;
-  return function(...args) {
-    const now = Date.now();
-    if (now - lastCall >= limit) {
-      lastCall = now;
-      func.apply(this, args);
-    }
-  };
 }
