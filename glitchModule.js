7714d6f7ad34134c58f4dc061ee1206a4f54f484
Joshua Esk <joshua.esk@icloud.com>
2025-05-20 16:17:13 +1000

    Update glitchModule.js

--- glitchModule.js
+++ glitchModule.js
@@ -1,13 +1,17 @@
 // glitchModule.js
 export function startGlitch() {
   const glitchOverlay = document.getElementById('glitch-overlay');
-  function triggerGlitch() {
+  if (!glitchOverlay) return;
+  
+  const triggerGlitch = () => {
     glitchOverlay.classList.add('glitch-active');
     setTimeout(() => {
       glitchOverlay.classList.remove('glitch-active');
     }, 300);
+    // Next glitch between 2000ms and 7000ms
     const nextGlitch = Math.random() * 5000 + 2000;
     setTimeout(triggerGlitch, nextGlitch);
-  }
+  };
+
   triggerGlitch();
 }
