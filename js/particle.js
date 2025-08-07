8db89e789f9169c78ed2b76b307890ed790a65a9
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 20:22:10 +1000

    Update particle.js

--- js/particle.js
+++ js/particle.js
@@ -2,45 +2,18 @@ class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.vx = (Math.random() - 0.5) * 2;
-    this.vy = (Math.random() - 0.5) * 2;
-    this.radius = 2;
-    this.damping = 0.01;
-    this.attraction = 0.0002;
-    this.maxSpeed = 2.5;
+    this.vx = (Math.random() - 0.5) * 0.5;
+    this.vy = (Math.random() - 0.5) * 0.5;
+    this.radius = 1.5;
   }
 
-  update(center, mouse) {
-    // Gentle central attraction
-    const dx = center.x - this.x;
-    const dy = center.y - this.y;
-    this.vx += dx * this.attraction;
-    this.vy += dy * this.attraction;
-
-    // Soft mouse repulsion
-    const mx = this.x - mouse.x;
-    const my = this.y - mouse.y;
-    const dist = Math.sqrt(mx * mx + my * my);
-    if (dist < 150) {
-      const force = (150 - dist) / 3000;
-      this.vx += mx * force;
-      this.vy += my * force;
-    }
-
-    // Damping
-    this.vx *= 1 - this.damping;
-    this.vy *= 1 - this.damping;
-
-    // Velocity clamping
-    const speed = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
-    if (speed > this.maxSpeed) {
-      const scale = this.maxSpeed / speed;
-      this.vx *= scale;
-      this.vy *= scale;
-    }
-
+  update(width, height) {
     this.x += this.vx;
     this.y += this.vy;
+
+    // Bounce off edges
+    if (this.x < 0 || this.x > width) this.vx *= -1;
+    if (this.y < 0 || this.y > height) this.vy *= -1;
   }
 
   draw(ctx) {
