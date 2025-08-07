910c47122549ae0087a267a52221eb2137f223a0
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 18:58:15 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,5 +5,6 @@ export const CONFIG = Object.freeze({
   dotColor: '#0f0',
   lineBaseAlpha: 0.2,
   pullStrength: 0.0005,
-  gradientRadius: 120
+  gradientRadius: 120,
+  sinkRadius: 160 // only particles within this radius are affected
 });
