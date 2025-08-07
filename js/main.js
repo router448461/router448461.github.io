567210f272a26755b6f42144093e7a35e598212d
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 20:22:35 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,26 +10,18 @@ window.addEventListener('resize', resize);
 resize();
 
 const particles = [];
-const center = { x: width / 2, y: height / 2 };
-const mouse = { x: center.x, y: center.y };
-
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 80; i++) {
   const x = Math.random() * width;
   const y = Math.random() * height;
   particles.push(new Particle(x, y));
 }
-
-canvas.addEventListener('mousemove', e => {
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
 
 function animate() {
   ctx.clearRect(0, 0, width, height);
 
   for (let i = 0; i < particles.length; i++) {
     const p = particles[i];
-    p.update(center, mouse);
+    p.update(width, height);
     p.draw(ctx);
 
     for (let j = i + 1; j < particles.length; j++) {
