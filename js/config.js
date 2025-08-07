396bb4b5d36ca557f6fe7de5f8244c91742d4f40
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 19:36:07 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,13 @@
 export const CONFIG = {
   particleCount: 120,
   dotColor: 'rgba(0, 255, 0, 0.8)',
-  linkDistance: 100,
   sinkRadius: 120,
   gradientRadius: 200,
-  gravityStrength: 0.05,
-  gravityFalloff: 300,
+  magneticStrength: 0.05,
+  magneticFalloff: 300,
   damping: 0.98,
   bounceLoss: 0.7,
   maxVelocity: 100,
   initialSpeed: 30,
-  tetherDistance: 300 // NEW: max distance for radial lines
+  tetherDistance: 300
 };
