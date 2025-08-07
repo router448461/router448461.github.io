56fe22c09ada87cbd9e145a57e797236b14f54c2
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 19:09:12 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,5 +6,9 @@ export const CONFIG = Object.freeze({
   lineBaseAlpha: 0.2,
   pullStrength: 0.0005,
   gradientRadius: 120,
-  sinkRadius: 160 // only particles within this radius are affected
+  sinkRadius: 160,
+  trailFade: 0.05,
+  pulseInterval: 3000,
+  driftVector: { x: 0.02, y: 0.01 },
+  gridSpacing: 80
 });
