b8a4d3f42e79840e602e35ee02dd22198f5efcbb
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 20:14:05 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { Tendril } from './tendril.js';
-
 const canvas = document.getElementById('background');
 const ctx = canvas.getContext('2d');
 let width, height;
@@ -11,17 +9,13 @@ function resize() {
 window.addEventListener('resize', resize);
 resize();
 
-const tendrils = [];
-const origin = { x: width / 2, y: height / 2 };
-const mouse = { x: origin.x, y: origin.y };
+const particles = [];
+const mouse = { x: width / 2, y: height / 2 };
 
-for (let i = 0; i < 30; i++) {
-  tendrils.push(new Tendril(origin, {
-    length: 25,
-    spacing: 8 + Math.random() * 4,
-    damping: 0.1 + Math.random() * 0.05,
-    attraction: 0.15 + Math.random() * 0.05
-  }));
+for (let i = 0; i < 100; i++) {
+  const x = Math.random() * width;
+  const y = Math.random() * height;
+  particles.push(new Particle(x, y));
 }
 
 canvas.addEventListener('mousemove', e => {
@@ -31,10 +25,17 @@ canvas.addEventListener('mousemove', e => {
 
 function animate() {
   ctx.clearRect(0, 0, width, height);
-  for (const tendril of tendrils) {
-    tendril.update(mouse);
-    tendril.draw(ctx);
+
+  for (let i = 0; i < particles.length; i++) {
+    const p = particles[i];
+    p.update(mouse);
+    p.draw(ctx);
+
+    for (let j = i + 1; j < particles.length; j++) {
+      Line.draw(ctx, p, particles[j]);
+    }
   }
+
   requestAnimationFrame(animate);
 }
 animate();
