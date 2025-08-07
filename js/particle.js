8df3daefaa269ed4fb1808922756f32a0bd6b710
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 18:30:41 +1000

    Create particle.js

---
+++ js/particle.js
@@ -0,0 +1,27 @@
+import { ctx, width, height } from './canvas.js';
+import { CONFIG } from './config.js';
+
+export class Particle {
+  constructor() {
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+    this.vx = (Math.random() - 0.5) * CONFIG.maxVelocity;
+    this.vy = (Math.random() - 0.5) * CONFIG.maxVelocity;
+    this.radius = 1 + Math.random() * 2;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x < 0 || this.x > width)  this.vx *= -1;
+    if (this.y < 0 || this.y > height) this.vy *= -1;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = CONFIG.dotColor;
+    ctx.fill();
+  }
+}
