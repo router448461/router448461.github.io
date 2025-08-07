1c151340db485040243beb200942d5d3f5d1c0cd
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 18:42:43 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
-export const CONFIG = {
+export const CONFIG = Object.freeze({
   particleCount: 100,
   maxVelocity: 0.5,
   connectionDistance: 120,
-  dotColor: '#0f0',
-  lineBaseAlpha: 0.2
-};
+  dotColor: '#0f0', // military green
+  lineBaseAlpha: 0.2,
+  formationSpacing: 80,
+  rogueRatio: 0.1,
+  pulseChance: 0.005
+});
