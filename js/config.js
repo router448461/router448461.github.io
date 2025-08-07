5604a2a7232f369423c8f24b9a8a748c6c3d5c33
Joshua Esk <joshua.esk@icloud.com>
2025-08-07 21:04:13 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,20 +1,17 @@
 // js/config.js
-
 export default {
   pixelRatio: window.devicePixelRatio || 1,
 
-  // grid & connection settings
   grid: {
-    cellSize: 50,
-    connectThreshold: 55
+    cellSize: 60,             // size of each logical grid cell
+    connectThreshold: 80      // max distance to draw a line
   },
 
-  // particle appearance & physics
   particle: {
     radius: 1.5,
     color: '#00ff00',
     drag: 0.98,
-    pulseAmplitude: 0.6,  // extra radius
-    pulseSpeed: 2         // cycles per second
+    pulseAmplitude: 0.6,      // how big the dots pulse
+    pulseSpeed: 1.5           // pulses per second
   }
 };
