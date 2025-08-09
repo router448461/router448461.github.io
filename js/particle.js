72ce8bcfc3e9298cde552e72d67bcce30ffb6f0e
Joshua Esk <joshua.esk@icloud.com>
2025-08-09 19:54:56 +1000

    Update particle.js

--- js/particle.js
+++ js/particle.js
@@ -1,15 +1,13 @@
 export class Particle {
   constructor(width, height, cfg) {
-    this.W   = width;
-    this.H   = height;
+    this.W = width;
+    this.H = height;
     this.cfg = cfg;
 
-    // start at random position
     this.x = Math.random() * this.W;
     this.y = Math.random() * this.H;
 
-    // velocity vector
-    const angle = Math.random() * Math.PI * 2;
+    const angle = Math.random() * 2 * Math.PI;
     const speed = cfg.baseSpeed + (Math.random() - 0.5) * cfg.speedVariance;
     this.vx = Math.cos(angle) * speed;
     this.vy = Math.sin(angle) * speed;
@@ -21,15 +19,17 @@ export class Particle {
     this.x += this.vx;
     this.y += this.vy;
 
-    // bounce off edges
-    if (this.x < 0 || this.x > this.W) this.vx *= -1;
-    if (this.y < 0 || this.y > this.H) this.vy *= -1;
+    if (this.x <= 0 || this.x >= this.W) this.vx *= -1;
+    if (this.y <= 0 || this.y >= this.H) this.vy *= -1;
   }
 
   draw(ctx) {
+    ctx.shadowBlur = this.cfg.glowBlur;
+    ctx.shadowColor = this.cfg.glowColor;
     ctx.fillStyle = this.cfg.particleColor;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
     ctx.fill();
+    ctx.shadowBlur = 0;
   }
 }
