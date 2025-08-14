b1e8c4d6baf854c1cf450088217030bf38c4e587
joshua esk <joshua.esk@icloud.com>
2025-08-14 16:56:08 +1000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-import { removeLoader } from './engine.init.js';
-import { startConstellation } from './engine.visuals.js';
+import { removeLoader } from "./engine.init.js";
+import { startConstellation2D } from "./engine.visuals.2d.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(removeLoader, 1000);
   setTimeout(() => {
-    startConstellation();
+    startConstellation2D();
   }, 1000);
 });
