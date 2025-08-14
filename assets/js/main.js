3600e08c3fcdfeb3c1691e8ed481eb1fff53dbc4
joshua esk <joshua.esk@icloud.com>
2025-08-14 17:26:06 +1000

    Update and rename main.js to engine.base.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { removeLoader } from "./engine.init.js";
-import { startConstellation2D } from "./engine.visuals.2d.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(removeLoader, 1000);
-  setTimeout(() => {
-    startConstellation2D();
-  }, 1000);
-});
